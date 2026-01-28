e42cb9169a95bf171882cb8cbbc297e070b723f5
INIT <etimi5319@gmail.com>
2026-01-28 02:55:13 -1200

    minor fixes, left for me to go through code

--- utils/util.py
+++ utils/util.py
@@ -21,7 +21,7 @@ def get_password_hash(password):
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
+    expire = datetime.now() + timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
